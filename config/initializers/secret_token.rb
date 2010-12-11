56a6b1a34901e5d46cadb47b80fb85c39fefa284
slouis <louis.sebastien@gmail.com>
2010-12-11 22:52:37 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajl::Application.config.secret_token = '00e099930ef1d08347891473356d703bd3539385a22e9ab9433ab426ae90a587cf8b876457fa1009c433a9b5915c556d9ea6c862bde6a7f3f1f2563410562a4c'
